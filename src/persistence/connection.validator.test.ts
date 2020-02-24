53c1def67c1c7339e37511da9f6775a5d7931e82
Edmond O'Flynn <oflynned@tcd.ie>
2020-02-24 11:20:08 +0100

    remove function declaration in favour of fat arrows

--- src/persistence/connection.validator.test.ts
+++ src/persistence/connection.validator.test.ts
@@ -17,7 +17,7 @@ describe('Connection Validator', () => {
                 expect(validator.validate(options)).toEqual("mongodb://username:password@host:1234/database")
             });
 
-            xit('should throw error on invalid port', function () {
+            xit('should throw error on invalid port', () => {
                 const options: ConnectionOptions = {
                     uri: "mongodb://username:password@host:port/database"
                 };
@@ -25,12 +25,12 @@ describe('Connection Validator', () => {
             });
         });
 
-        describe('with auth', function () {
-            it('should validate without username and password', function () {
+        describe('with auth', () => {
+            it('should validate without username and password', () => {
                 expect(validator.validate(baseOptions)).toEqual("mongodb://host:1234/database")
             });
 
-            it('should validate with username and password', function () {
+            it('should validate with username and password', () => {
                 const options: ConnectionOptions = {
                     username: "username",
                     password: "password",
@@ -39,12 +39,12 @@ describe('Connection Validator', () => {
                 expect(validator.validate(options)).toEqual("mongodb://username:password@host:1234/database")
             });
 
-            it('should require both username and password to prefill connection uri', function () {
+            it('should require both username and password to prefill connection uri', () => {
                 expect(validator.validate({username: "username", ...baseOptions})).toEqual("mongodb://host:1234/database");
                 expect(validator.validate({password: "password", ...baseOptions})).toEqual("mongodb://host:1234/database")
             });
 
-            it('should escape special characters in the password string', function () {
+            it('should escape special characters in the password string', () => {
                 const options: ConnectionOptions = {
                     username: "username",
                     password: "p@ssword",
@@ -54,7 +54,7 @@ describe('Connection Validator', () => {
             });
         });
 
-        it('should prefer uri string', function () {
+        it('should prefer uri string', () => {
             const options: ConnectionOptions = {
                 uri: "mongodb://username1:password1@host1:12345/database1",
                 ...baseOptions
