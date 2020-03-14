b7e1dc89fac5e1c93ce8086f27113db357ed27c1
Edmond O'Flynn <oflynned@tcd.ie>
2020-03-14 18:30:02 +0100

    update example

--- src/example/credential-validation.example.ts
+++ src/example/credential-validation.example.ts
@@ -1,20 +1,21 @@
 import MongoClient, {ConnectionOptions} from "../persistence/mongo.client";
 import User from "./models/user";
+import {Repository} from "../models/documents/repository";
 
 const main = async (client: MongoClient) => {
+    await Repository.deleteMany(User, client);
+
     const user: User = await new User().build({
         name: "John Smith",
         email: "email@test.com",
         password: "password"
     });
 
-    console.log(user);
-
     // contains plaintext password field, hash field is undefined
     console.log(user.toJson().password, user.toJson().passwordHash);
-    const record = await user.save(client);
 
     // pre-validation hook scrubs the password field and sets the hashed field on the committed db record
+    const record = await user.save(client);
     console.log(record.toJson().password, record.toJson().passwordHash);
 
     // pre-validation hook also removes the .password field on the user instance since it should not be needed anymore
@@ -23,16 +24,22 @@ const main = async (client: MongoClient) => {
     // we can also still compare credentials on the instance without direct password comparison
     console.log('does this password attempt match?', await user.passwordAttemptMatches("not the password"));
     console.log('does this password attempt match?', await user.passwordAttemptMatches("password"));
+
 };
 
 (async () => {
     const options: ConnectionOptions = {
         host: 'localhost',
         port: 27017,
-        database: 'test'
+        database: 'mongoize'
     };
 
     const client = await new MongoClient(options).connect();
-    await main(client);
-    await client.close();
+    try {
+        await main(client);
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
 })();
