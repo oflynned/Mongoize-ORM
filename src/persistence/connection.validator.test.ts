0b4fc283479ab9b4e192061d9694e1bac13ccdcc
Edmond O'Flynn <oflynned@tcd.ie>
2020-03-14 17:09:14 +0100

    implement regex splitting of mongodb connection string in validator w/ tests

--- src/persistence/connection.validator.test.ts
+++ src/persistence/connection.validator.test.ts
@@ -14,20 +14,57 @@ describe('Connection Validator', () => {
                 const options: ConnectionOptions = {
                     uri: "mongodb://username:password@host:1234/database"
                 };
-                expect(validator.validate(options)).toEqual("mongodb://username:password@host:1234/database")
+                validator.validate(options);
+                expect(validator.options.uri).toEqual("mongodb://username:password@host:1234/database")
             });
 
-            xit('should throw error on invalid port', () => {
+            it('should throw error on invalid port', () => {
                 const options: ConnectionOptions = {
                     uri: "mongodb://username:password@host:port/database"
                 };
-                expect(validator.validate(options)).toThrowError(Error("invalid connection option"))
+                expect(() => validator.validate(options)).toThrow("bad connection string passed")
+            });
+
+            it('should require database', () => {
+                const options: ConnectionOptions = {
+                    uri: "mongodb://username:password@host:port"
+                };
+                expect(() => validator.validate(options)).toThrow("bad connection string passed")
+            });
+
+            it('should parse connection string with credentials', () => {
+                const options: ConnectionOptions = {
+                    uri: "mongodb://username:password@host:1234/database"
+                };
+
+                validator.validate(options);
+                expect(validator.options.uri).toEqual("mongodb://username:password@host:1234/database");
+                expect(validator.options.username).toEqual("username");
+                expect(validator.options.password).toEqual("password");
+                expect(validator.options.host).toEqual("host");
+                expect(validator.options.port).toEqual(1234);
+                expect(validator.options.database).toEqual("database")
+            });
+
+            it('should parse connection string with no credentials', () => {
+                const options: ConnectionOptions = {
+                    uri: "mongodb://host:1234/database"
+                };
+
+                validator.validate(options);
+                expect(validator.options.uri).toEqual("mongodb://host:1234/database");
+                expect(validator.options.username).toBeUndefined();
+                expect(validator.options.password).toBeUndefined();
+                expect(validator.options.host).toEqual("host");
+                expect(validator.options.port).toEqual(1234);
+                expect(validator.options.database).toEqual("database")
             });
         });
 
         describe('with auth', () => {
             it('should validate without username and password', () => {
-                expect(validator.validate(baseOptions)).toEqual("mongodb://host:1234/database")
+                validator.validate(baseOptions);
+                expect(validator.options.uri).toEqual("mongodb://host:1234/database")
             });
 
             it('should validate with username and password', () => {
@@ -36,12 +73,15 @@ describe('Connection Validator', () => {
                     password: "password",
                     ...baseOptions
                 };
-                expect(validator.validate(options)).toEqual("mongodb://username:password@host:1234/database")
+                validator.validate(options);
+                expect(validator.options.uri).toEqual("mongodb://username:password@host:1234/database")
             });
 
             it('should require both username and password to prefill connection uri', () => {
-                expect(validator.validate({username: "username", ...baseOptions})).toEqual("mongodb://host:1234/database");
-                expect(validator.validate({password: "password", ...baseOptions})).toEqual("mongodb://host:1234/database")
+                validator.validate({username: "username", ...baseOptions});
+                expect(validator.options.uri).toEqual("mongodb://host:1234/database");
+                validator.validate({password: "password", ...baseOptions});
+                expect(validator.options.uri).toEqual("mongodb://host:1234/database")
             });
 
             it('should escape special characters in the password string', () => {
@@ -50,7 +90,8 @@ describe('Connection Validator', () => {
                     password: "p@ssword",
                     ...baseOptions
                 };
-                expect(validator.validate(options)).toEqual('mongodb://username:p%40ssword@host:1234/database');
+                validator.validate(options);
+                expect(validator.options.uri).toEqual('mongodb://username:p%40ssword@host:1234/database');
             });
         });
 
@@ -59,7 +100,8 @@ describe('Connection Validator', () => {
                 uri: "mongodb://username1:password1@host1:12345/database1",
                 ...baseOptions
             };
-            expect(validator.validate(options)).toEqual("mongodb://username1:password1@host1:12345/database1")
+            validator.validate(options);
+            expect(validator.options.uri).toEqual("mongodb://username1:password1@host1:12345/database1")
         });
     });
 });
--- src/persistence/connection.validator.ts
+++ src/persistence/connection.validator.ts
@@ -13,18 +13,64 @@ type AuthConnectionOptions = {
 export type ConnectionOptions = Partial<UriConnectionOptions> & Partial<AuthConnectionOptions>;
 
 export class ConnectionValidator {
-    validate(connection: ConnectionOptions): string {
+
+    options: ConnectionOptions;
+
+    validate(connection: ConnectionOptions): void {
         if (connection.uri) {
-            // TODO should split uri into auth connections options style with regex instead and throw an error
-            //      in the case of passing a non-numeric port number
-            return connection.uri;
+            const {uri, username, password, host, port, database} = this.parseUriString(connection.uri);
+            this.options = {
+                uri, username, password, host, port, database
+            };
+            return;
         }
 
         if (connection.username && connection.password) {
             const safeEncodedPassword = encodeURIComponent(connection.password);
-            return `mongodb://${connection.username}:${safeEncodedPassword}@${connection.host}:${connection.port}/${connection.database}`
+            this.options = {
+                uri: `mongodb://${connection.username}:${safeEncodedPassword}@${connection.host}:${connection.port}/${connection.database}`,
+                ...connection
+            };
+            return;
         }
 
-        return `mongodb://${connection.host}:${connection.port}/${connection.database}`
+        this.options = {
+            uri: `mongodb://${connection.host}:${connection.port}/${connection.database}`,
+            ...connection
+        }
+    }
+
+    private parseUriString(uri: string): ConnectionOptions {
+        const regex = new RegExp(/^(mongodb:(?:\/{2})?)((\w+?):(\w+?)@|:?@?)(\w+?):(\d+)\/(\w+?)$/);
+        const fields = uri.split(regex);
+
+        if (fields.length < 8) {
+            throw new Error("bad connection string passed")
+        }
+
+        /**
+         * [ '',
+             'mongodb://',
+             'username:password@',
+             'username',
+             'password',
+             'host',
+             '1234',
+             'database',
+             '' ]
+         */
+        const [, protocol, , username, password, host, port, database] = fields;
+
+        // only supporting mongodb type
+        if (protocol !== "mongodb://") {
+            throw new Error("invalid protocol -- needs to be mongodb:// type")
+        }
+
+        // a connection string needs to have
+        if (!(host || port || database)) {
+            throw new Error("missing connection string field");
+        }
+
+        return {uri, username, password, host, port: parseInt(port), database}
     }
 }
