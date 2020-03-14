d111bf336d9fb1a1a21c311eb474c51e9db0420d
Edmond O'Flynn <oflynned@tcd.ie>
2020-03-14 17:23:13 +0100

    first pass with working db insertion with real mongodb

--- src/example/credential-validation.example.ts
+++ src/example/credential-validation.example.ts
@@ -13,17 +13,16 @@ const main = async (client: MongoClient) => {
     // contains plaintext password field, hash field is undefined
     console.log(user.toJson().password, user.toJson().passwordHash);
     const record = await user.save(client);
-    console.log(record);
 
     // pre-validation hook scrubs the password field and sets the hashed field on the committed db record
-    // console.log(record.toJson().password, record.toJson().passwordHash);
+    console.log(record.toJson().password, record.toJson().passwordHash);
 
     // pre-validation hook also removes the .password field on the user instance since it should not be needed anymore
-    // console.log(user.toJson().password, user.toJson().passwordHash);
+    console.log(user.toJson().password, user.toJson().passwordHash);
 
     // we can also still compare credentials on the instance without direct password comparison
-    // console.log('does this password attempt match?', await user.passwordAttemptMatches("not the password"));
-    // console.log('does this password attempt match?', await user.passwordAttemptMatches("password"));
+    console.log('does this password attempt match?', await user.passwordAttemptMatches("not the password"));
+    console.log('does this password attempt match?', await user.passwordAttemptMatches("password"));
 };
 
 (async () => {
