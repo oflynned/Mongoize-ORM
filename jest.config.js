d49ff18887af772d7cacc2c067c11db43b87e4e0
Edmond O'Flynn <oflynned@tcd.ie>
2020-04-05 13:56:11 +0200

    omit src/example from coverage

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     "^.+\\.ts?$": "ts-jest"
   },
   collectCoverage: true,
-  collectCoverageFrom: ["./src/**/*.ts"],
+  collectCoverageFrom: ["src/**/*.ts", "!src/example/**/*"],
   moduleFileExtensions: ["ts", "js", "json"],
   coverageDirectory: "./coverage",
   testMatch: ["**/*.{unit,integration}.ts"],
